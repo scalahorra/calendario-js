20abda859b1f5d292add9c42f4c54a8e7953c319
scalahorra <71397714+scalahorra@users.noreply.github.com>
2022-01-25 15:52:01 +0100

    funciones de los botones

--- script.js
+++ script.js
@@ -3,10 +3,32 @@ let clicked = null;
 let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
 
 const calendar = document.getElementById('calendar');
+const newEventModal = document.getElementById('newEventModal');
+const backDrop = document.getElementById('modalBackDrop');
+const eventTitleInput = document.getElementById('eventTitleInput'); 
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+function openModal(date) {
+    clicked = date;
+
+    const eventForDay = events.find(e => e.date === clicked);
+
+    if(eventForDay) {
+        console.log('El evento ya existe')
+
+    } else {
+        newEventModal.style.display = 'block';
+    }
+
+    backDrop.style.display = 'block';
+}
 
 function load() {
     const dt = new Date();
+
+    if(nav !== 0) {
+        dt.setMonth(new Date().getMonth() + nav);
+    }
     
     const day = dt.getDate();
     const month = dt.getMonth();
@@ -25,7 +47,9 @@ function load() {
     const paddingDays = weekdays.indexOf(dateString.split(', ') [0]);
 
     document.getElementById('monthDisplay').innerText = 
-        `${dt.toLocaleDateString('en-us', {month: 'long'})} ${year}`
+        `${dt.toLocaleDateString('en-us', {month: 'long'})} ${year}`;
+
+    calendar.innerHTML = '';
 
     for(let i = 1; i <= paddingDays + daysInMonth; i++) {
         const daySquare = document.createElement('div');
@@ -33,7 +57,7 @@ function load() {
 
         if(i > paddingDays) {
             daySquare.innerText = i - paddingDays;
-            daySquare.addEventListener('click', () => console.log('click'));
+            daySquare.addEventListener('click', () => openModal(`${i - paddingDays}/${month + 1}/${year}`));
 
         } else {
             daySquare.classList.add('padding');
@@ -43,12 +67,30 @@ function load() {
     }
 }
 
+function closeModal() {
+    newEventModal.style.display = 'none';
+    backDrop.style.display = 'none';
+    eventTitleInput.value = '';
+    clicked = null;
+    load();
+}
+
 function initButtons() {
     document.getElementById('nextButton').addEventListener('click', () => {
         nav++;
-        
-    })
+        load();
+    });
+
+    document.getElementById('backButton').addEventListener('click', () => {
+        nav--;
+        load();
+    });
+
+    document.getElementById('saveButton').addEventListener('click', saveEvent);
+    document.getElementById('cancelButton').addEventListener('click', closeModal);
 }
 
 initButtons();
 load();
+
+/* 46:54 */
