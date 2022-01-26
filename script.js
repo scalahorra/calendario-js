a1478c82fad8d981acb797432ad0928013717019
scalahorra <71397714+scalahorra@users.noreply.github.com>
2022-01-26 01:01:13 +0100

    guardar y eliminar eventos

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('e
 
 const calendar = document.getElementById('calendar');
 const newEventModal = document.getElementById('newEventModal');
+const deleteEventModal = document.getElementById('deleteEventModal');
 const backDrop = document.getElementById('modalBackDrop');
 const eventTitleInput = document.getElementById('eventTitleInput'); 
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -14,8 +15,8 @@ function openModal(date) {
     const eventForDay = events.find(e => e.date === clicked);
 
     if(eventForDay) {
-        console.log('El evento ya existe')
-
+        document.getElementById('eventText').innerText = eventForDay.title;
+        deleteEventModal.style.display = 'block';
     } else {
         newEventModal.style.display = 'block';
     }
@@ -55,9 +56,24 @@ function load() {
         const daySquare = document.createElement('div');
         daySquare.classList.add('day');
 
+        const dayString = `${i - paddingDays}/${month + 1}/${year}`;
+
         if(i > paddingDays) {
             daySquare.innerText = i - paddingDays;
-            daySquare.addEventListener('click', () => openModal(`${i - paddingDays}/${month + 1}/${year}`));
+            const eventForDay = events.find(e => e.date === dayString);
+
+            if(i - paddingDays === day && nav === 0) {
+                daySquare.id = 'currentDay';
+            }
+
+            if(eventForDay) {
+                const eventDiv = document.createElement('div');
+                eventDiv.classList.add('event');
+                eventDiv.innerText = eventForDay.title;
+                daySquare.appendChild(eventDiv);
+            }
+
+            daySquare.addEventListener('click', () => openModal(dayString));
 
         } else {
             daySquare.classList.add('padding');
@@ -68,11 +84,37 @@ function load() {
 }
 
 function closeModal() {
+    eventTitleInput.classList.remove('error');
     newEventModal.style.display = 'none';
+    deleteEventModal.style.display = 'none';
     backDrop.style.display = 'none';
     eventTitleInput.value = '';
     clicked = null;
     load();
+}
+
+function saveEvent() {
+    if(eventTitleInput.value) {
+        eventTitleInput.classList.remove('error');
+
+        events.push({
+            date: clicked,
+            title: eventTitleInput.value
+        });
+
+        localStorage.setItem('events', JSON.stringify(events));
+
+        closeModal();
+
+    } else {
+        eventTitleInput.classList.add('error');
+    }
+}
+
+function deleteEvent() {
+    events = events.filter(e => e.date !== clicked);
+    localStorage.setItem('events', JSON.stringify(events));
+    closeModal();
 }
 
 function initButtons() {
@@ -88,9 +130,9 @@ function initButtons() {
 
     document.getElementById('saveButton').addEventListener('click', saveEvent);
     document.getElementById('cancelButton').addEventListener('click', closeModal);
+    document.getElementById('deleteButton').addEventListener('click', deleteEvent);
+    document.getElementById('closeButton').addEventListener('click', closeModal);
 }
 
 initButtons();
 load();
-
-/* 46:54 */
